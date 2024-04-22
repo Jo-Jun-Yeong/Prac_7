a3174a0447381e449a2fb5b3a05649270c02aa83
test1 <idwnsdud1965@naver.com>
2024-04-23 01:29:25 +0900

    first

---
+++ src/main/java/com/example/mini_Cimmute/MiniCimmuteApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_Cimmute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCimmuteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCimmuteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_Cimmute/MiniCimmuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_Cimmute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniCimmuteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
